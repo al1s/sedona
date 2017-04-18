8c1c961b7a6a2eae3c8b161d9e6bf8e0c08d01d3
Alstof <alstof@gmail.com>
2017-04-18 13:35:12 -0700

    Add menu animation and validate html

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,5 +1,48 @@
+// Datepick element for the search hotel form
 $( document ).ready(function() {
   $(function() {
     $('.is-datepick').datepick();
   });
 });
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+      $('nav')
+        .addClass('menu--hidden');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+          $('nav')
+            .removeClass('menu--hidden');
+        }
+    }
+
+    lastScrollTop = st;
+}
